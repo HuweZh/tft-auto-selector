cacd4bca6205962dc00600791792e17f3a8f5d07
HuweZh <1241671281@qq.com>
2023-06-09 21:29:01 +0800

    初次提交

---
+++ src/main/java/com/huhusw/tftautoselector/TftAutoSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.huhusw.tftautoselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftAutoSelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TftAutoSelectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huhusw/tftautoselector/TftAutoSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huhusw.tftautoselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftAutoSelectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
