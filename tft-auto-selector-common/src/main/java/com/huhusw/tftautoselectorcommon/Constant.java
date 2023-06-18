0c08a4e2248cb1bfbdc390a3182d39fd436cb612
HuweZh <1241671281@qq.com>
2023-06-18 16:56:02 +0800

    界面测试

--- tft-auto-selector-common/src/main/java/com/huhusw/tftautoselectorcommon/Constant.java
+++ tft-auto-selector-common/src/main/java/com/huhusw/tftautoselectorcommon/Constant.java
@@ -1,29 +1,74 @@
 package com.huhusw.tftautoselectorcommon;
 
+import java.util.*;
+
 public class Constant {
     /**
      * 一费卡
      */
-    public static String COST_ONE = "一费";
+    public static final String COST_ONE = "一费";
 
     /**
      * 二费卡
      */
-    public static String COST_TWO = "二费";
+    public static final String COST_TWO = "二费";
 
     /**
      * 三费卡
      */
-    public static String COST_THREE = "三费";
+    public static final String COST_THREE = "三费";
 
     /**
      * 四费卡
      */
-    public static String COST_FOUR = "四费";
+    public static final String COST_FOUR = "四费";
 
     /**
      * 五费卡
      */
-    public static String COST_FIVE = "五费";
+    public static final String COST_FIVE = "五费";
 
+    /**
+     * 一费英雄
+     */
+    public static final List<String> HERO_ONE = new ArrayList<>(Arrays.asList(
+            "凯尔", "崔丝塔娜", "科加斯", "艾瑞莉娅", "茂凯", "雷克顿", "奥利安娜", "卡西奥佩娅", "波比", "马尔扎哈", "烬", "佛耶戈", "莎弥拉"));
+
+    /**
+     * 二费英雄
+     */
+    public static final List<String> HERO_TWO = new ArrayList<>(Arrays.asList(
+            "加里奥", "索拉卡", "提莫", "沃里克", "艾希", "卡萨丁", "斯维因", "塔莉垭", "金克斯", "劫", "克烈", "蔚", "瑟提"));
+
+
+    /**
+     * 三费英雄
+     */
+    public static final List<String> HERO_THREE = new ArrayList<>(Arrays.asList(
+            "娑娜", "卡尔玛", "塔里克", "卡特琳娜", "盖伦", "德莱厄斯", "杰斯", "丽桑卓", "维克兹", "阿克尚", "艾克", "雷克赛", "卡莉斯塔"));
+
+    /**
+     * 四费英雄
+     */
+    public static final List<String> HERO_FOUR = new ArrayList<>(Arrays.asList(
+            "厄加特", "嘉文四世", "内瑟斯", "慎", "拉克丝", "瑟庄妮", "卡莎", "亚索", "泽丽", "阿兹尔", "厄斐琉斯", "格温"));
+
+    /**
+     * 五费英雄
+     */
+    public static final List<String> HERO_FIVE = new ArrayList<>(Arrays.asList(
+            "瑞兹", "塞恩", "黑默丁格", "阿狸", "卑尔维斯", "赛娜", "亚托克斯", "奎桑提"));
+
+    /**
+     * 费用对应的英雄列表
+     */
+    public static final Map<String, List<String>> COST_TO_ALL_HERO = new HashMap<>();
+
+    static {
+        COST_TO_ALL_HERO.put(COST_ONE, HERO_ONE);
+        COST_TO_ALL_HERO.put(COST_TWO, HERO_TWO);
+        COST_TO_ALL_HERO.put(COST_THREE, HERO_THREE);
+        COST_TO_ALL_HERO.put(COST_FOUR, HERO_FOUR);
+        COST_TO_ALL_HERO.put(COST_FIVE, HERO_FIVE);
+    }
 }
--- tft-auto-selector-ocr/src/main/java/com/huhusw/tftautoselector/screen/Screen.java
+++ tft-auto-selector-ocr/src/main/java/com/huhusw/tftautoselector/screen/Screen.java
@@ -1,10 +1,13 @@
 package com.huhusw.tftautoselector.screen;
 
 import com.huhusw.tftautoselectorcommon.Constant;
+import org.springframework.util.StringUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Screen extends JFrame {
 
@@ -34,13 +37,10 @@ public class Screen extends JFrame {
     private JComboBox<String> heroSelector;
 
 
-
     /**
      * 选择的英雄
      */
     private String heroName;
-
-    private final String COST_ONE = "";
 
     public Screen() {
         jPanel = new JPanel();
@@ -59,25 +59,10 @@ public class Screen extends JFrame {
         jPanel.add(costSelector);
 
         heroSelector = new JComboBox<>();
-        heroSelector.addItem("五费");
-        heroSelector.addItem("四费");
-        heroSelector.addItem("三费");
-        heroSelector.addItem("二费");
-        heroSelector.addItem("一费");
-        heroSelector.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("下拉事件");
-                System.out.println(e.getActionCommand());
-                System.out.println(e.getModifiers());
-                System.out.println(e.getWhen());
-
-                System.out.println(heroSelector.getEditor().getItem());
-            }
-        });
+        this.setHeroInfo(null);
         jPanel.add(heroSelector);
 
-        button = new JButton("开始");
+        button = new JButton("自动选择");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -85,6 +70,58 @@ public class Screen extends JFrame {
             }
         });
         jPanel.add(button);
+    }
+
+    /**
+     * 英雄信息
+     *
+     * @param costType 费用类型
+     */
+    private void setHeroInfo(String costType) {
+        heroSelector.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                heroName = (String) heroSelector.getEditor().getItem();
+            }
+        });
+        List<String> heroNames = new ArrayList<>();
+        if (!StringUtils.hasLength(costType)) {
+            for (List<String> heroes : Constant.COST_TO_ALL_HERO.values()) {
+                heroNames.addAll(heroes);
+            }
+            this.fillHeroInfo(heroNames);
+            return;
+        }
+
+        switch (costType) {
+            case Constant.COST_FIVE:
+                this.fillHeroInfo(Constant.COST_TO_ALL_HERO.get(Constant.COST_FIVE));
+                break;
+            case Constant.COST_FOUR:
+                this.fillHeroInfo(Constant.COST_TO_ALL_HERO.get(Constant.COST_FOUR));
+                break;
+            case Constant.COST_THREE:
+                this.fillHeroInfo(Constant.COST_TO_ALL_HERO.get(Constant.COST_THREE));
+                break;
+            case Constant.COST_TWO:
+                this.fillHeroInfo(Constant.COST_TO_ALL_HERO.get(Constant.COST_TWO));
+                break;
+            case Constant.COST_ONE:
+                this.fillHeroInfo(Constant.COST_TO_ALL_HERO.get(Constant.COST_ONE));
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 填充英雄选择框的英雄信息
+     *
+     * @param heroes 英雄名称
+     */
+    private void fillHeroInfo(List<String> heroes) {
+        for (String hero : heroes) {
+            heroSelector.addItem(hero);
+        }
     }
 
     /**
@@ -99,15 +136,74 @@ public class Screen extends JFrame {
         costSelector.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("下拉事件");
-                System.out.println(e.getActionCommand());
-                System.out.println(e.getModifiers());
-                System.out.println(e.getWhen());
-
-                System.out.println(costSelector.getEditor().getItem());
+                // 清除所有的item
+                heroSelector.removeAllItems();
+                // 添加新的item
+                if (costSelector.getEditor().getItem().equals(Constant.COST_FIVE)) {
+                    setHeroInfo(Constant.COST_FIVE);
+                } else if (costSelector.getEditor().getItem().equals(Constant.COST_FOUR)) {
+                    setHeroInfo(Constant.COST_FOUR);
+                } else if (costSelector.getEditor().getItem().equals(Constant.COST_THREE)) {
+                    setHeroInfo(Constant.COST_THREE);
+                } else if (costSelector.getEditor().getItem().equals(Constant.COST_TWO)) {
+                    setHeroInfo(Constant.COST_TWO);
+                } else if (costSelector.getEditor().getItem().equals(Constant.COST_ONE)) {
+                    setHeroInfo(Constant.COST_ONE);
+                } else {
+                    setHeroInfo(null);
+                }
             }
         });
     }
 
+    public JPanel getjPanel() {
+        return jPanel;
+    }
 
+    public void setjPanel(JPanel jPanel) {
+        this.jPanel = jPanel;
+    }
+
+    public JButton getButton() {
+        return button;
+    }
+
+    public void setButton(JButton button) {
+        this.button = button;
+    }
+
+    public JComboBox<String> getCostSelector() {
+        return costSelector;
+    }
+
+    public void setCostSelector(JComboBox<String> costSelector) {
+        this.costSelector = costSelector;
+    }
+
+    public JComboBox<String> getHeroSelector() {
+        return heroSelector;
+    }
+
+    public void setHeroSelector(JComboBox<String> heroSelector) {
+        this.heroSelector = heroSelector;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    @Override
+    public String toString() {
+        return "Screen{" +
+                "jPanel=" + jPanel +
+                ", button=" + button +
+                ", costSelector=" + costSelector +
+                ", heroSelector=" + heroSelector +
+                ", heroName='" + heroName + '\'' +
+                '}';
+    }
 }
